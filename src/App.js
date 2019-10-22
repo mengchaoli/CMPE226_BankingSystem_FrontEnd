3c2f3dc820ba9ecd68f7ac2c66a9786954a9a33f
mengchaoli <lmcwesdxc@163.com>
2019-10-22 00:24:18 -0700

    login page
    added axios and material-ui;
    rewrite CustomerList.js;

--- src/App.js
+++ src/App.js
@@ -1,70 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
+//This is my own design: car background pic + nav bar + userlist
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import {Button, Card, Col, Icon, Row, Typography} from "antd";
+// import Pic from "./assets/960x0.jpg";
+// import CustomMenu from "./Menu";
+// import FilterTag from "./FilterTag";
+// import CustomerList from "./CustomerList";
+//
+// class App extends React.Component {
+//
+//   state = {
+//     contentOne: 'Text One',
+//     key: '1',
+//   }
+//
+//   handleChangeContentOne(menuKey) {
+//     switch(menuKey) {
+//       case "":
+//     }
+//     this.setState({
+//       contentOne: `New Content ${Math.random() * 100}`
+//
+//     });
+//   }
+//
+//   render() {
+//     const { contentOne, key } = this.state;
+//     return (
+//         <>
+//         <Card bodyStyle={{ backgroundImage: `url(${Pic})`, height: 500, width: '100%',
+//           backgroundSize:'100% 100%',
+//           backgroundRepeat: 'no-repeat',
+//           backgroundPosition: 'top center' }}>
+//           <Row type='flex' justify='space-around'>
+//             <h2 style={{color: 'white'}}> Header </h2>
+//           </Row>
+//         </Card>
+//         <Card>
+//         <Row gutter={32}>
+//
+//         <Col span={12}>
+//           <CustomMenu changeContent={this.handleChangeContentOne.bind(this)} />
+//         {/*<div style={{border: '1px solid #0d0d9d'}}>Card One</div>*/}
+//         </Col>
+//
+//         <Col span={12}>
+//           <Row>
+//             <Typography.Title level={4}> Title </Typography.Title>
+//             <Typography.Paragraph> {contentOne} </Typography.Paragraph>
+//           </Row>
+//
+//           <Row>
+//             {/*<Typography.Text type='secondary'>Text Two</Typography.Text>*/}
+//             {/*<Button type='link' size='small' icon='linkedin' />*/}
+//             {/*<Icon type="linkedin" theme="filled" style={{color: 'green'}} />*/}
+//             <FilterTag />
+//           </Row>
+//           <Row>
+//             <CustomerList />
+//           </Row>
+//         </Col>
+//         </Row>
+//         </Card>
+//           </>
+//     );
+//   }
+// }
+//
+// export default App;
+
+//This is login part
+import React, { Component } from 'react';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
 import './App.css';
-import {Button, Card, Col, Icon, Row, Typography} from "antd";
-import Pic from "./assets/960x0.jpg";
-import CustomMenu from "./Menu";
-import FilterTag from "./FilterTag";
-import CustomerList from "./CustomerList";
+import Loginscreen from './Loginscreen'
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+//injectTapEventPlugin();
 
-class App extends React.Component {
-
-  state = {
-    contentOne: 'Text One',
-    key: '1',
-  }
-
-  handleChangeContentOne(menuKey) {
-    switch(menuKey) {
-      case "":
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            loginPage:[],
+            uploadScreen:[]
+        }
     }
-    this.setState({
-      contentOne: `New Content ${Math.random() * 100}`
-
-    });
-  }
-
-  render() {
-    const { contentOne, key } = this.state;
-    return (
-        <>
-        <Card bodyStyle={{ backgroundImage: `url(${Pic})`, height: 500, width: '100%',
-          backgroundSize:'100% 100%',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'top center' }}>
-          <Row type='flex' justify='space-around'>
-            <h2 style={{color: 'white'}}> Header </h2>
-          </Row>
-        </Card>
-        <Card>
-        <Row gutter={32}>
-
-        <Col span={12}>
-          <CustomMenu changeContent={this.handleChangeContentOne.bind(this)} />
-        {/*<div style={{border: '1px solid #0d0d9d'}}>Card One</div>*/}
-        </Col>
-
-        <Col span={12}>
-          <Row>
-            <Typography.Title level={4}> Title </Typography.Title>
-            <Typography.Paragraph> {contentOne} </Typography.Paragraph>
-          </Row>
-
-          <Row>
-            {/*<Typography.Text type='secondary'>Text Two</Typography.Text>*/}
-            {/*<Button type='link' size='small' icon='linkedin' />*/}
-            {/*<Icon type="linkedin" theme="filled" style={{color: 'green'}} />*/}
-            <FilterTag />
-          </Row>
-          <Row>
-            <CustomerList />
-          </Row>
-        </Col>
-        </Row>
-        </Card>
-          </>
-    );
-  }
+    componentWillMount(){
+        var loginPage =[];
+        loginPage.push(<Loginscreen parentContext={this}/>);
+        this.setState({
+            loginPage:loginPage
+        })
+    }
+    render() {
+        return (
+            <div className="App">
+                {this.state.loginPage}
+                {this.state.uploadScreen}
+            </div>
+        );
+    }
 }
-
+const style = {
+    margin: 15,
+};
 export default App;
--- src/CustomerList.js
+++ src/CustomerList.js
@@ -1,70 +1,165 @@
 import React from 'react';
-import { List, Avatar, Icon } from 'antd';
+// import { List, Avatar, Icon } from 'antd';
+//
+// const listData = [];
+// for (let i = 0; i < 23; i++) {
+//     listData.push({
+//         href: 'http://ant.design',
+//         title: `customer ${i}`,
+//         avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+//         description:
+//             'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+//         content:
+//             'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+//     });
+// }
+//
+// const IconText = ({ type, text }) => (
+//     <span>
+//     <Icon type={type} style={{ marginRight: 8 }} />
+//         {text}
+//   </span>
+// );
+// class CustomerList extends React.Component {
+//     render() {
+//         return (
+//             <List
+//                 itemLayout="vertical"
+//                 size="large"
+//                 pagination={{
+//                     onChange: page => {
+//                         console.log(page);
+//                     },
+//                     pageSize: 3,
+//                 }}
+//                 dataSource={listData}
+//                 footer={
+//                     <div>
+//                         <b>ant design</b> footer part
+//                     </div>
+//                 }
+//                 renderItem={item => (
+//                     <List.Item
+//                         key={item.title}
+//                         actions={[
+//                             <IconText type="star" text="24" key="list-vertical-star-o" />,
+//                             <IconText type="like-o" text="156" key="list-vertical-like-o" />,
+//                             <IconText type="message" text="2" key="list-vertical-message" />,
+//                         ]}
+//                         // extra={
+//                         //     <img
+//                         //         width={272}
+//                         //         // alt="logo"
+//                         //         // src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+//                         //     />
+//                         // }
+//                     >
+//                         <List.Item.Meta
+//                             avatar={<Avatar src={item.avatar} />}
+//                             title={<a href={item.href}>{item.title}</a>}
+//                             description={item.description}
+//                         />
+//                         {item.content}
+//                     </List.Item>
+//                 )}
+//             />
+//                 // mountNode,
+//         );
+//     }
+// }
+// export default CustomerList;
 
-const listData = [];
-for (let i = 0; i < 23; i++) {
-    listData.push({
-        href: 'http://ant.design',
-        title: `ant design part ${i}`,
-        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-        description:
-            'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-        content:
-            'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-    });
-}
+import { List, message, Avatar, Spin } from 'antd';
+import reqwest from 'reqwest';
 
-const IconText = ({ type, text }) => (
-    <span>
-    <Icon type={type} style={{ marginRight: 8 }} />
-        {text}
-  </span>
-);
+import InfiniteScroll from 'react-infinite-scroller';
+
+const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
+
 class CustomerList extends React.Component {
+    state = {
+        data: [],
+        loading: false,
+        hasMore: true,
+    };
+
+    componentDidMount() {
+        this.fetchData(res => {
+            this.setState({
+                data: res.results,
+            });
+        });
+    }
+
+    fetchData = callback => {
+        reqwest({
+            url: fakeDataUrl,
+            type: 'json',
+            method: 'get',
+            contentType: 'application/json',
+            success: res => {
+                callback(res);
+            },
+        });
+    };
+
+    handleInfiniteOnLoad = () => {
+        let { data } = this.state;
+        this.setState({
+            loading: true,
+        });
+        if (data.length > 14) {
+            message.warning('Infinite List loaded all');
+            this.setState({
+                hasMore: false,
+                loading: false,
+            });
+            return;
+        }
+        this.fetchData(res => {
+            data = data.concat(res.results);
+            this.setState({
+                data,
+                loading: false,
+            });
+        });
+    };
+
     render() {
         return (
-            <List
-                itemLayout="vertical"
-                size="large"
-                pagination={{
-                    onChange: page => {
-                        console.log(page);
-                    },
-                    pageSize: 3,
-                }}
-                dataSource={listData}
-                footer={
-                    <div>
-                        <b>ant design</b> footer part
-                    </div>
-                }
-                renderItem={item => (
-                    <List.Item
-                        key={item.title}
-                        actions={[
-                            <IconText type="star-o" text="156" key="list-vertical-star-o" />,
-                            <IconText type="like-o" text="156" key="list-vertical-like-o" />,
-                            <IconText type="message" text="2" key="list-vertical-message" />,
-                        ]}
-                        extra={
-                            <img
-                                width={272}
-                                alt="logo"
-                                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                            />
-                        }
+            <div className="demo-infinite-container">
+                <InfiniteScroll
+                    initialLoad={false}
+                    pageStart={0}
+                    loadMore={this.handleInfiniteOnLoad}
+                    hasMore={!this.state.loading && this.state.hasMore}
+                    useWindow={false}
+                >
+                    <List
+                        dataSource={this.state.data}
+                        renderItem={item => (
+                            <List.Item key={item.id}>
+                                <List.Item.Meta
+                                    avatar={
+                                        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                                    }
+                                    title={<a href="https://ant.design">{item.name.last}</a>}
+                                    description={item.email}
+                                />
+                                <div>Content</div>
+                            </List.Item>
+                        )}
                     >
-                        <List.Item.Meta
-                            avatar={<Avatar src={item.avatar} />}
-                            title={<a href={item.href}>{item.title}</a>}
-                            description={item.description}
-                        />
-                        {item.content}
-                    </List.Item>
-                )}
-            />
-                // mountNode,
+                        {this.state.loading && this.state.hasMore && (
+                            <div className="demo-loading-container">
+                                <Spin />
+                            </div>
+                        )}
+                    </List>
+                </InfiniteScroll>
+            </div>
         );
     }
 }
+
 export default CustomerList;
